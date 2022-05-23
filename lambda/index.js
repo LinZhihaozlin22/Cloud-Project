f5d9dfe274efc9356945aa36bdb4165f80ad8f91
Hsuan Wang <jessica.w@inwinstack.com>
2022-05-23 13:19:15 -0700

    update lambda function

--- lambda/index.js
+++ lambda/index.js
@@ -3,17 +3,35 @@ var DBHandler = require("./DBHandler")
 exports.handler = (event, context, callback) => {
     console.log('This event is ' + JSON.stringify(event))
     var intent = event.currentIntent.name;
-    var slot = event.currentIntent.slots.Location;
-
+    var storeloc = event.currentIntent.slots.Location;
+    
     DBHandler.getRestaurantDetails(intent, function(err, data) {
         if (err) {
             context.fail(err);
         } else {
-            let output = '';
-            if (data.Item.message !== undefined)
-            output = data.Item.message;
-            else
-            output = data.Item.Slot[slot]
+            let output = intent;
+            if (intent == 'WorkingHours') {
+                output = data.Item.message;
+            } else if (intent == 'restaurantLocation') {
+                output = data.Item.Slot[storeloc]
+            } else {
+                // var storeloc = event.currentIntent.slots.Location;
+                var bookDate = event.currentIntent.slots.Date;
+                var bookTime = event.currentIntent.slots.Time.split(':');
+                var partySize = parseInt(event.currentIntent.slots.Size);
+                
+                if (parseInt(bookTime[0])<9 || parseInt(bookTime[0]) >21) {
+                    output = 'Our open hour is 9:00 to 21:00, please enter a correct booking time.'
+                } else{
+                    if (data.Item.Slot[storeloc]['capacity']-data.Item.Slot[storeloc]['reserved'] < partySize) {
+                        output = 'Sorry, we don\'t have enough seats for this reservation.'
+                    } else {
+                    
+                        output = "Okay, your table for " + partySize +" people will be ready on " + bookTime[0] + ":" + bookTime[1] + " at " + bookDate + ".\nThe address is " + data.Item.Slot[storeloc]['address'] + ". Thank you!"
+                    }
+                }
+            }
+        
             var response = {
                 "dialogAction": {
                     "type": "Close",
@@ -27,5 +45,5 @@ exports.handler = (event, context, callback) => {
             callback(null, response);
         }
     });
-
+    
 };
